505f410bb0cadd53df84ebac9ff0f98702729a0d
Ziling Zhao <zilingzhao@gmail.com>
2011-03-06 03:23:18 -0800

    playable music

--- subsonic-cli.py
+++ subsonic-cli.py
@@ -2,6 +2,9 @@
 
 import simplejson
 
+import mad
+import pyaudio
+
 import clisonic
 
 USER="test"
@@ -11,15 +14,40 @@ URL="https://localhost/subsonic"
 def prettyPrint(json):
     print simplejson.dumps(json, sort_keys=True, indent=4 * ' ')
 
+class SubsonicPlayer(object):
+
+    def __init__(self):
+        self.sub = clisonic.SubsoniceAPI(URL, username=USER, password=PW)
+
+        ret = self.sub.call_search(query={"artist": "Yelle"})
+
+        prettyPrint(ret)
+        self.play(ret["match"])
+
+    def play(self, song):
+
+        sub_stream = self.sub.call_stream(query={"id": song["id"]})
+
+        mf = mad.MadFile(sub_stream)
+
+        p = pyaudio.PyAudio()
+
+        stream = p.open(format=p.get_format_from_width(pyaudio.paInt32),
+                channels = 2,
+                rate = mf.samplerate(),
+                output = True)
+
+        data = mf.read()
+
+        while data != None:
+            stream.write(data)
+            data = mf.read()
+
+        stream.close()
+        p.terminate()
 
 def main():
-    sub = clisonic.SubsoniceAPI(URL, username=USER, password=PW)
-
-    ret = sub.call_search(query={"artist": "Yelle"})
-
-    song_id = ret["match"][0]["id"]
-
-    resp = sub.call_stream(query={"id": song_id})
+    SubsonicPlayer()
 
 if __name__ == "__main__":
     main()
